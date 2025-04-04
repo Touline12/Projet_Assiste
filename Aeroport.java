95ff152b3bceec39761696d4c26a46f3a7611969
Anthony Yazbeck <antounyaz@gmail.com>
2025-04-04 14:55:31 +0200

    Attribut de class Aeroport rajoutes

---
+++ Aeroport.java
@@ -0,0 +1,13 @@
+public class Aeroport {
+
+    private String nom;
+    private String ville;
+    private String description;
+
+    public Aeroport(String nom, String ville, String description) {
+        this.nom = nom;
+        this.ville = ville;
+        this.description = description;
+    }
+
+}
