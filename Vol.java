2183f078079e3fce384fb471877ea983424dc7a0
Anthony Yazbeck <antounyaz@gmail.com>
2025-04-04 14:54:09 +0200

    Attribut de class Vol rajoutes

---
+++ Vol.java
@@ -0,0 +1,17 @@
+public class Vol {
+
+    private int numeroVol;
+    private String origine;
+    private String destination;
+    private String dateHeureDepart;
+    private String dateHeureArrivee;
+
+    public Vol(int numeroVol, String origine, String destination, String dateHeureDepart, String dateHeureArrivee) {
+        this.numeroVol = numeroVol;
+        this.origine = origine;
+        this.destination = destination;
+        this.dateHeureDepart = dateHeureDepart;
+        this.dateHeureArrivee = dateHeureArrivee;
+    }
+
+}
