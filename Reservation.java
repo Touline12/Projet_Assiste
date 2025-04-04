04f0a4862b7c874d21d0b8dc29d5ee0b32fb8eef
Anthony Yazbeck <antounyaz@gmail.com>
2025-04-04 15:06:47 +0200

    Attributs de class Reservation rajoutes

---
+++ Reservation.java
@@ -0,0 +1,13 @@
+public class Reservation {
+
+    private int numeroReservation;
+    private String dateReservation;
+    private String statut;
+
+    public Reservation(int numeroReservation, String dateReservation, String statut) {
+        this.numeroReservation = numeroReservation;
+        this.dateReservation = dateReservation;
+        this.statut = statut;
+    }
+
+}
